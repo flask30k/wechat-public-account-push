1ac0412261a0383ff29ba3359fde77e7e200b7bb
flask30k <34526337+flask30k@users.noreply.github.com>
2023-01-16 21:08:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '蒙蒙',
+      name: '懵懵o','来者可追'
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqr2p5tpnq9J5a-CqRYyIM9hRlmg','oqr2p5hPfmjeRvn1ZO5C9OofR9mE'
       // 使用微信测试号：你想对他发送的模板消息的模板ID
