cd957531ed33cb97e37573c6a0c3bf2ef0ef64e2
flask30k <34526337+flask30k@users.noreply.github.com>
2023-01-16 21:16:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,22 +29,17 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '生日', name: '蒙蒙', year: '2002', date: '11-28',,
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '蒙蒙', year: '2002', date: '11-28',
-        },
-        {
-          type: '节日', name: '全权', year: '1999', date: '12-02',
+          type: '生日', name: '全全', year: '1999', date: '12-02',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2020-10-28' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
