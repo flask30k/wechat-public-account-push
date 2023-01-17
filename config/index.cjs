0644d1314fb3dad891ccd218bae63655c58f7ab5
flask30k <34526337+flask30k@users.noreply.github.com>
2023-01-17 13:45:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqr2p5hPfmjeRvn1ZO5C9OofR9mE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'izGYsaeJCCLssBgAtfXzxSxHNd7ibfVEs-oCdezLaDE',
+      useTemplateId: 'MxYfIyhWSb9vohnZ_kt8fyOsix_b0ICqLLGkfXSxiwE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-28',
       horoscopeDateType: '今日',
