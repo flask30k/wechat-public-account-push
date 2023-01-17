b401e2750aa463decddd1735eecc4f94cbda6901
flask30k <34526337+flask30k@users.noreply.github.com>
2023-01-17 15:20:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,11 +20,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '来者可追',
+      name: '懵懵o',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqr2p5hPfmjeRvn1ZO5C9OofR9mE',
+      id: 'oqr2p5tpnq9J5a-CqRYyIM9hRlmg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'RUbIO1Bv9MClSuPKjlgAYRezq5wHyfuqzsWNWTvpM5o',
+      useTemplateId: 'MxYfIyhWSb9vohnZ_kt8fyOsix_b0ICqLLGkfXSxiwE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-28',
       horoscopeDateType: '今日',
@@ -48,7 +48,7 @@ const USER_CONFIG = {
 
       
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'RUbIO1Bv9MClSuPKjlgAYRezq5wHyfuqzsWNWTvpM5o',
+  CALLBACK_TEMPLATE_ID: 'MxYfIyhWSb9vohnZ_kt8fyOsix_b0ICqLLGkfXSxiwE',
   CALLBACK_USERS: [
     {
       name: '自己',
